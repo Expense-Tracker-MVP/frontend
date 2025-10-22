6ae089827bfb62f11d2ad2aacd5d15162a4c63aa
john <ypoh007@e.ntu.edu.sg>
2025-10-22 21:05:16 +0800

    Added Expenses page.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,11 @@ import ThemeToggle from './ThemeToggle'
 
 const Navbar = () => {
   return (
-     <div className="flex gap-1.5 justify-around p-4 bg-[var(--primary)]" >
+     <div className="flex gap-1.5 justify-around items-center p-4 bg-[var(--primary)]" >
         <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" href="/sign-in">Sign In</Link>
         <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" href="/sign-up">Sign Up</Link>
         <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" href="/profile">Profile</Link>
+        <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" href="/expenses">Expenses</Link>
         <ThemeToggle />
       </div>
   )
