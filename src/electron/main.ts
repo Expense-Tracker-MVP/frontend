32db0af17f6a729a3f729106c53fff19a12d5b7e
john <ypoh007@e.ntu.edu.sg>
2025-11-09 17:17:49 +0800

    Modified electron main process in order to run it in development.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,5 +1,12 @@
 import { app, BrowserWindow } from "electron";
-import path from "path";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({
@@ -10,8 +17,15 @@ app.on("ready", () => {
             contextIsolation: true,
         }
     });
-    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
-        .catch(err => {
-            console.error('Failed to load index.html:', err);
-        });
+
+    if (process.env.NODE_ENV === 'development') {
+        console.log('Loading URL for development');
+        mainWindow.loadURL('http://localhost:5173');
+    }
+    else {
+        mainWindow.loadFile(path.join(__dirname, '../dist-react/index.html'))
+            .catch(err => {
+                console.error('Failed to load index.html:', err);
+            });
+    }
 });
