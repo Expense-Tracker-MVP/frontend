b90ec6552ef61f90fc4b7e1d984454dd64ae58bd
john <ypoh007@e.ntu.edu.sg>
2025-11-09 13:46:08 +0800

    Remove redundant boilerplate code.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/ui/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
