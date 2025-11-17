731d31b0098d229b33f400c01a72e5306a362035
john <ypoh007@e.ntu.edu.sg>
2025-11-17 20:11:58 +0800

    Implemented conditional rendering of navbar for authenticated and unauthenticated.

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -1,17 +1,26 @@
-
 import { Link } from 'react-router'
 import ThemeToggle from '@ui/components/ThemeToggle'
+import { useAuthStore } from '../lib/store/authStore'
 
 const Navbar = () => {
+    const { isAuthenticated } = useAuthStore()
     return (
         <div className="flex gap-1.5 justify-around items-center p-4 bg-[var(--primary)]" >
             <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/">Home</Link>
             <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/about">About</Link>
-            <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/sign-in">Sign In</Link>
-            <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/sign-up">Sign Up</Link>
-            <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/profile">Profile</Link>
-            <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/expenses">Expenses</Link>
-            <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/visuals">Visuals</Link>
+            {isAuthenticated ? null : (
+                <>
+                    <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/sign-in">Sign In</Link>
+                    <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/sign-up">Sign Up</Link>
+                </>
+            )}
+            {!isAuthenticated ? null : (
+                <>
+                    <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/profile">Profile</Link>
+                    <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/expenses">Expenses</Link>
+                    <Link className="text-1.5xl font-bold hover:underline text-gray-600 dark:text-gray-100" to="/visuals">Visuals</Link>
+                </>
+            )}
             <ThemeToggle />
         </div>
     )
